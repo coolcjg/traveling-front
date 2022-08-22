7dbc8725dc3bb6145fed8f84b621bfa624bd565d
coolcjg <coolcjg@naver.com>
2022-08-22 23:26:18 +0900

    아이디 체크 추가

--- src/JoinForm.js
+++ src/JoinForm.js
@@ -6,23 +6,40 @@ const JoginForm = () => {
 
     useEffect(() =>{
 
-        document.getElementById('id').addEventListener('focusout', ()=>{
-            checkId();
+        document.getElementById('id').addEventListener('focusout', (e)=>{
+            checkId(e.target.value);
         });
 
-        function checkId(){
+        function checkId(id){
 
             axios({
-                url: '', // 통신할 웹문서
-                method: 'get', // 통신할 방식
-                data: { // 인자로 보낼 데이터
-                  foo: 'diary'
+                url: 'http://localhost:5000/user/checkId', // 통신할 웹문서
+                method: 'post', // 통신할 방식
+                headers: { 'content-type': 'application/json', 'Pragma': 'no-cache'},
+                dataType: "json",
+                data: { 
+                  id: id,
                 }
-              });            
+
+            }).then(response =>{
+                if(response.data.result >= 1){
+                    document.getElementById('idSpan').textContent ='이미 존재하는 아이디입니다.';
+                }else{
+                    document.getElementById('idSpan').textContent ='';
+                };
+
+            })
 
         }
-
     },[])
+
+    function checkPassword(){
+        if(document.getElementById('password1').value !== document.getElementById('password2').value){
+            document.getElementById('passwordSpan').textContent ='비밀번호를 다시 확인해주세요.';
+        }else{
+            document.getElementById('passwordSpan').textContent ='';
+        }
+    }    
 
     return(
         <>
@@ -38,29 +55,43 @@ const JoginForm = () => {
 
                     <div className='inputContainer'>
 
-                        <label htmlFor='id'>아이디 </label>
                         <div>
-                        <input type='text' id='id'></input>
+                            <label htmlFor='id'>아이디 </label>
+                            <div>
+                            <input type='text' id='id'></input>
+                            </div>
+                            <span id='idSpan'></span>
                         </div>
                         
-                        <label htmlFor='password1'>비밀번호 </label>
                         <div>
-                        <input type='password1' id='password1'></input>
+                            <label htmlFor='password1'>비밀번호 </label>
+                            <div>
+                            <input type='password1' id='password1'></input>
+                            </div>
                         </div>
 
-                        <label htmlFor='password2'>비밀번호 확인 </label>
                         <div>
-                        <input type='password2' id='password2'></input>                        
+                            <label htmlFor='password2'>비밀번호 확인 </label>
+                            <div>
+                            <input type='password2' id='password2' onBlur={checkPassword}></input>
+                            </div>
+                            <span id='passwordSpan'></span>
                         </div>
 
-                        <label htmlFor='name'>이름 </label>
                         <div>
-                        <input type='name' id='name'></input>                        
+                            <label htmlFor='name'>이름 </label>
+                            <div>
+                            <input type='name' id='name'></input>
+                            </div>
+                            <span>aaa</span>
                         </div>
 
-                        <label htmlFor='email'>이메일 </label>
                         <div>
-                        <input type='email' id='email'></input>                        
+                            <label htmlFor='email'>이메일 </label>
+                            <div>
+                            <input type='email' id='email'></input>                        
+                            </div>
+                            <span>bbb</span>
                         </div>
 
                     </div>
