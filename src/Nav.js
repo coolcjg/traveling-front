339ecf00dd3bfd81c91d2be2bdc5d46290c762e5
coolcjg <coolcjg@naver.com>
2022-08-30 23:10:43 +0900

    로그인 분기처리 완료

--- src/Nav.js
+++ src/Nav.js
@@ -1,6 +1,8 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 const Nav = () => {
+
+    const [accessToken, setAccessToken] = useState('');
 
     function loginForm(e){
         e.preventDefault();
@@ -10,7 +12,19 @@ const Nav = () => {
     function joinForm(e){
         e.preventDefault();
         window.location.href='/joinForm';
-    }    
+    }
+
+    function logout(){
+        localStorage.removeItem('accessToken');
+        setAccessToken(null);
+    }
+
+    useEffect(()=>{
+        console.log("실행1");
+        console.log('accessToken1 : ' + accessToken);
+        setAccessToken(localStorage.getItem('accessToken'));
+        console.log('accessToken2 : ' + accessToken);
+    }, [accessToken]);
 
     return(
         <nav className='nav'>
@@ -22,11 +36,14 @@ const Nav = () => {
                     <li className='item'><a href="/">동영상</a></li>
                 </ul>
             </div>
-
+            { accessToken === null ?
             <div className='loginContainer'>
-                <button className='navButton' onClick={joinForm}>가입</button>
-                <button className='navButton' onClick={loginForm}>로그인</button>
-            </div>
+            <button className='navButton' onClick={joinForm}>가입</button>
+            <button className='navButton' onClick={loginForm}>로그인</button>
+            </div> : 
+            <button className='navButton' onClick={logout}>로그아웃</button>
+            }
+
         </nav>
     );
 }
